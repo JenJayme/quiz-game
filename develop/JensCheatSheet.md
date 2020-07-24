f2b141341b9fcbba2e791a6105d775448a237231
Jen Jayme <jeniferjayme@gmail.com>
2020-07-24 16:33:45 -0700

    Corrected lots of syntax errors, reworked front page, reworked ShowQuestionDiv function.

---
+++ develop/JensCheatSheet.md
@@ -0,0 +1,122 @@
+# To link a style sheet
+    <link rel="stylesheet" href="assets/css/style.css">
+
+# To link a script.js file
+    <script type="text/javascript" src="script.js"></script>
+
+# To link to jQuery CDN
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+    </script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+
+# To get something programed in script.js to print on the DOM
+    $("#idordivinindex").append(varname);
+
+# To get a list programed in script.js to print on the DOM
+First create the unordered list tags:
+    USE JQUERY: uli = $('<ul'><'ul'>);   
+    OR USE JAVASCRIPT: 
+        uli = document.createElement('ul');
+        ul.addClass('my-list')
+
+Then populate the list with a for loop:
+    for loop {
+        li1 = $('<li'><'li'>);
+        li1.addClass('list-item');
+        li.text(name);
+        ul1.append(li1);
+        $("#idordivonindex").append(varname);
+        }
+
+# To create a for loop
+    for (var i = o; i < variable.length; i++) {
+        insert function here (element to pass thru function);
+    }
+
+# To point a var to an id or class on index.html
+//Set a reference to a container with Welcome message and set the contents to the message.
+        var questionDiv = document.querySelector(".questionDiv");
+        console.log("questionsContainer: " + questionsContainer);
+    };
+
+
+# array of objects
+var QandA = [
+    {
+        question: "For what sport was Abraham Lincoln enshrined in a Sports Hall of Fame for having a stellar record of just one loss?",
+        answers: ["baseball", ",football", "wrestling", "soccer"],
+        rightAnswer: 3
+    },{
+        question: "Which NBA basketball legend appeared in a movie with Bruce Lee?",
+        answers: ["Wilt Chamberlain", "Kareem Abdul-Jabbar", "Julius Erving", "Michael Jordan"],
+        rightAnswer: 2
+    },{
+
+# Add To Do Items
+
+
+# Render To Do Items
+
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript", ];
+
+renderTodos();
+
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    todoList.appendChild(li);
+  }
+}
+
+// todos.push("Pet the cat");
+// renderTodos();
+
+# Set Attributes to change styling with Javascript
+// we used querySelector and querySelectorAll to gather our elements into variables rather than using the more specific document methods.
+// querySelector because it allows us to select an element by tag, class name, or ID. This makes it more flexible than using getElementById or getElementByClassName.
+// querySelectorAll because it allows us to select all elements of the same tag, class name, or ID. This is much more useful than getElementsByTagName or other group selectors.
+// These two methods are the most useful and versatile because they can be used to target any element we would ever like to target.
+
+
+var pTags = document.querySelectorAll("p");
+var divTags = document.querySelectorAll("div");
+var aTags = document.querySelectorAll("a");
+var imgEl = document.querySelectorAll("img");
+var changeP = document.querySelector("#change2");
+
+console.log(pTags);
+
+
+// How was all of the styling done if there is no CSS?
+// All the styling done to the page uses the method, .setAttribute.
+// The setAttribute method takes in two arguments, first is the name of the attribute, and the second is its value. i.e., `setAttribute("style", "color:white;");
+
+// The major difference between setting styles with .setAttribute vs the .style methods is that when using setAttribute we enter the CSS properties and values like we would in a normal external stylesheet or inline styling.
+
+pTags[0].setAttribute("style", "font-size: 65px;");
+changeP.setAttribute("style", "color:blue; border:2px solid black;");
+aTags[0].setAttribute("href", "https://github.com");
+imgEl[0].setAttribute("src", "images/image_1.jpg");
+imgEl[0].setAttribute("style", "width:500px; height:200px;");
+
+for (var i = 0; i < divTags.length; i++) {
+  divTags[i].setAttribute("style", "text-decoration:underline; color:red;");
+}
+
+
+
+
+# Create an element on the DOM
--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 
 //Set up an array of objects with each object having a question, four possible answers, and one correct answer.  Identify the correct answer using index.
-
 var QandA = [
     {
         question: "For what sport was Abraham Lincoln enshrined in a Sports Hall of Fame for having a stellar record of just one loss?",
         answers: ["baseball", ",football", "wrestling", "soccer"],
-        rightAnswer: 3
-    },{
+        rightAnswer: 2
+    }, {
         question: "Which NBA basketball legend appeared in a movie with Bruce Lee?",
         answers: ["Wilt Chamberlain", "Kareem Abdul-Jabbar", "Julius Erving", "Michael Jordan"],
-        rightAnswer: 2
-    },{
+        rightAnswer: 1
+    }, {
         question: "Which of America's Founding Fathers had a house which was discovered to have about one thousand two hundred bone pieces from about ten human skeletons?",
         answers: ["John Adams", "George Washington", "Alexander Hamilton", "Benjamin Franklin"],
-        rightAnswer: 4
-    },{
+        rightAnswer: 3
+    }, {
         question: "What city was the first capital of the United States?",
         answers: ["New York NY", "Plymouth MA", "Jamestown VA", "Washington DC"],
-        rightAnswer: 1
-    },{
+        rightAnswer: 0
+    }, {
         question: "Who was first U.S. president to be impeached?",
         answers: ["Bill Clinton", "Andrew Johnson", "Richard Nixon", "Donald Trump"],
-        rightAnswer: 2
+        rightAnswer: 1
     }
 ];
 
@@ -38,7 +37,7 @@ var nextQuestion = 0;
 var award = 500;
 
 //Number of seconds to deduct for every wrong answer.
-var penalty = 15; 
+var penalty = 15;
 
 //Bucket for score, starts at 0.
 var score = 0;
@@ -51,97 +50,95 @@ var highScoreArray = [];
 
 //Create functions for three major tasks.
 
-    function showWelcomeMessage {
-//Connect the quiz section on the index.html with a variable pointing to a div.
-//Set a reference to a container with Welcome message and set the contents to the message.
-        var questionDiv = document.querySelector(".questionDiv");
-        console.log("questionsContainer: " + questionsContainer);
-    };
-
-    function startQuiz {
     //Get a reference to the container where this info will be displayed.
     //Reset timeLeft if its not at the default value. 
 
-        $("#startBtn").on("click", function() {
-            timeLeft = 75000;
-            startTimer();
-            showQuestion();
-        });
-    }
+function showQuestionAndAnswer(nextQuestion) {
+    var QNAObject, div, ul, li, button;
 
-    function showQuestionAndAnswer(index) {
-        var QNAObject, div, ul, li, button;
-        QNAObject = QandA[index];
+    for (var i = 0; i < QandA.length; i++) {
+        QNAObject = QandA[nextQuestion];
+        QNAObject.question = QandA[nextQuestion].question;
+        QNAObject.answers = QandA[nextQuestion].answers;
+        console.log(QNAObject);
 
         // Create Question div
         div = $('<div></div>');
         div.addClass('question');
         div.text(QNAObject.question);
-        questionsContainer.append(div);
+        console.log('Question: ' + QNAObject.question);
+        console.log('Answer: ' + QNAObject.answers);
+        $(questionsContainer).append(div);
 
         // Create Answer Div
-        div = $('<div></div>'); // <div></div>
-        div.addClass('asnwers'); // <div class="answers"></div>
-        ul = $('<ul></ul>');
+        // div = $('<div></div>'); 
+        // div.addClass('answers'); 
+        // ol = $('<ol></ol>');
 
         for (var i = 0; i < QNAObject.answers.length; i++) {
-          li = $('<li></li>');
-          button = $('<button></button>');
-          button.text(QNAObject.answer[i]);
-          button.on('click', function() {
-            checkAnswer(QNAObject, i);
-          })
-          li.append(button);
-          ul.append(li);
+            var answerOption = QNAObject.answers[i];
+            $(answerList).append(`<div class="col-2"><li><button class="btn btn-warning list-group-item" id="answerListItem">${answerOption}</button></li></div>`);
+            console.log(QNAObject.answers[i])
         }
-        
-        div.append(ul);
-        questionsContainer.append(div); // We are home
-        nextQuestion++; // 1
-      }
-
-      showQuestionAndAnswer(nextQuestion);
-
-    function callBackFunc() {
-        if (timeLeft === 0) {
-            stopTimer(sid);
-        } else {
-            --timeLeft;
-        }
-    };
-
-    function startTimer(callBackFunc, interval) {
-        //start the timer by using set interval()
-        sid = setInterval(callBackFunc, interval);
     }
 
-    function stoptimer() {
-        clearInterval(sid);
+    nextQuestion++;
+    console.log(nextQuestion);
+}
+
+function callBackFunc() {
+    if (timeLeft === 0) {
+        stopTimer(sid);
+    } else {
+        --timeLeft;
     }
+};
 
-    function checkAnswer(qao, i) {
-        if (qao.rightAnswer === i) {
-           //TODO: awward points
-           score = score + award
-        } else {
-          // TODO: deduct time from timer
-          timeLeft = timeLeft - penalty
-        }
-      }
+function startTimer() {
+    //start the timer by using set interval()
+    countdown('countdownID', 0, 0, 0, 75);
+    // // Countdown Loading Bar
+    // $config.loadingBars_width = 200;
+    // $config.loadingBars_height = 20;
+    // $config.loadingBars_border_color ='blue';
+    // $config.loadingBars_color = 'darkblue';
+    // $config.loadingBars_background_color = 'lightblue';
 
-    function viewHighScores {
+    // // Countdown Timer
+    // $config.timer_color ='blue';
+    // $config.timer_font_weight = 700;
+    // $config.timer_font ='Verdana';
+    // $config.timer_font_size = 12;
+    // $config.endtime_message ='Timer expired!';
+}
+
+
+function stoptimer() {
+    clearInterval(sid);
+}
+
+function checkAnswer(qao, chosen) {
+    if (qao.rightAnswer === chosen) {
+        //TODO: awward points
+        score = score + award
+    } else {
+        // TODO: deduct time from timer
+        timeLeft = timeLeft - penalty
+    }
+}
+
+function viewHighScores() {
     //Get a reference to the container where this info will be displayed.
-     //Retrieve saved high scores from local storage, if any.
+    //Retrieve saved high scores from local storage, if any.
     //Iterate through list of high scores and create HTML elements for name and score.
     //Set this list as the contents of the referenced container.
     //Hide all sibling containers and make this container visible.
     //Return values retrieved from local storage by key.
-    function getValuesFromLS(key) {
-    
-    }
+    // function getValuesFromLS(key) {
+    // }
 
     //store values in local storage under a key.
-    function saveToLS {
+    function saveToLS() {
         localStorage.setItem(LS_KEY, highScoreArray);
         //convert to string
         JSON.stringify(values)
@@ -149,4 +146,30 @@ var highScoreArray = [];
         //convert from string
         JSON.parse(values)
     }
-    };
+}
+
+function showOpeningMessage () {
+    $(welcomeDiv).text(
+    "Following are five questions related to the books and blockbuster HBO series. Once you click start, you'll have 75 seconds to choose from several answers. Easy right? Except, if you answer incorrectly, you'll get a time penalty and 15 seconds will be taken off the clock. Ready? Good luck!")
+    $(welcomeDiv).append(`<button id="rollQuestions" class="btn btn-warning">Start quiz</button>`);
+}
+
+
+function setup () {
+    $("answerListItem").on('click', function () {
+    checkAnswer(QNAObject, i);
+    });
+
+    $("#startBtn").on("click", function startQuiz () {
+        showOpeningMessage();
+        $('welcomeDiv').addClass('hidden');
+        $('hero-text').addClass('hidden');
+    });
+
+    $("#rollQuestions").on("click", function rollQueiz () {
+        startTimer();
+        showQuestionAndAnswer(nextQuestion);
+    })
+}
+
+setup();
